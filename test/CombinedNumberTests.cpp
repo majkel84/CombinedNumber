db756e8d3c282377099f13eb43f24111fb376361
majkel <michal.sobotka.84@gmail.com>
2019-02-12 21:49:05 +0100

    0 or 1 given Tests

--- test/CombinedNumberTests.cpp
+++ test/CombinedNumberTests.cpp
@@ -7,13 +7,13 @@ struct CombinedNumberTests : public ::testing::Test
     CombinedNumber cn;
 };
 
-TEST_F(CombinedNumberTests, whenEmptyListIsGivenEmptyStringShouldBeReturned)
+TEST_F(CombinedNumberTests, whenEmptyListGivenEmptyStringShouldBeReturned)
 {
     auto result = cn.constructGreatestNumber({});
     ASSERT_EQ(result, "");
 }
 
-TEST_F(CombinedNumberTests, when1IsGivenString1IsReturned)
+TEST_F(CombinedNumberTests, whenOneIsGivenStringOneIsReturned)
 {
     auto result = cn.constructGreatestNumber({1});
     ASSERT_EQ(result, "1");
